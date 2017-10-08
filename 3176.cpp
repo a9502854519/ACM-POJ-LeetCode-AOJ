78d901dd18720c1acf0fda1a38baab6f49c6126c
a9502854519 <a9502854519@gmail.com>
2017-10-08 11:33:38 +0800

    3176

---
+++ 3176.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#define MAX_N 350
+
+using namespace std;
+
+int dp[MAX_N+1][MAX_N+1];
+
+int  main(){
+    int N;
+    cin>>N;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i; j++){
+            scanf("%d", &dp[i][j]);
+            int temp = 0;
+            if(i > 0){
+                if(j < i) temp = max(temp, dp[i-1][j]);
+                if(j > 0) temp = max(temp, dp[i-1][j-1]);
+            }
+            dp[i][j] += temp;
+        }
+    }
+    int res = 0;
+    for(int i = 0; i < N; i++){
+        res = max(res, dp[N-1][i]);
+    }
+    cout<<res<<endl;
+    return 0;
+}
+
