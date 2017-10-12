9b1fbeb103f6770f7ea2a7d12990ba8928e272df
a9502854519 <a9502854519@gmail.com>
2017-10-12 13:10:34 +0800

    1082

--- 1082.cpp
+++ 1082.cpp
@@ -1,65 +1,71 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
-#define YEAR_OFFSET 1900
+#include<cstdlib>
 using  namespace std;
 
 int Y, M, D;
+const int date_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const static int YEAR_OFFSET = 1900; 
 
-int date_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-bool dp[102][13][32];
+struct Date{
+	int y, m, d, days;
 
-struct date{
-	int  y, m, d;
-	date() {}
-	date(int y, int m, int d) : y(y), m(m), d(d) {}
+	bool leap_date(){
+		return (m == 2 && ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)));
+	}
+	int get_day(){
+		return date_of_month[m] + leap_date();
+	}
+	void correct_month(){
+		while(m > 12){
+			m -= 12;
+			y++;
+		}
+	}
+	Date() : y(1900), m(1), d(1), days(31) {}
+	Date(int yy, int mm, int dd) : y(yy), m(mm), d(dd) {
+		//maybe something wrong here
+		correct_month();
+		while(d > (days = get_day())){
+			d -= days;
+			m++;
+			correct_month();
+		}
+	}
+
+	Date  operator + (int v){
+		return Date(y, m, d+v);
+	}
+	Date  operator << (int v){
+		Date temp(y, m+v, d);
+		if(temp.d != d) return Date(y, m, d+1);
+		return temp;
+	}
 };
-int leap_date(int i, int j){
-	i += YEAR_OFFSET;
-	return (i % 4 == 0 && (i % 400 == 0 || i % 100 != 0)) && j == 2;
-}
-date get_next_day(int i, int j, int k){
-	int days = date_of_month[j] + leap_date(i, j);
-	k = k % days + 1;
-	j = (j + (k == 1) - 1) % 12 + 1;
-	i += (k == 1 && j == 1);
-	return date(i, j, k);
-}
-date get_next_month(int i, int j, int k){
-	j = j % 12 + 1;
-	i += j == 1;
-	if(k > date_of_month[j] + leap_date(i, j)) i = -1;
-	return date(i, j, k);
+bool dp[102][13][32];
+void pause(){
+	fgetc(stdin);
 }
 void solve(){
-	memset(dp, false, sizeof(dp));
-	Y -= 1900;
-	int i = Y, j = M, k = D;
-	int days;
-	while(i <= 101){
-		printf("%d-%02d-%02d %s-->", i+1900, j, k,(dp[i][j][k] ? "true" : "false"));
-		date next_date = get_next_day(i, j, k);
-		if(next_date.y <= 101){
-		       	dp[next_date.y][next_date.m][next_date.d] |= !dp[i][j][k];
-			printf("\t%d-%02d-%02d  %s\n", next_date.y+1900, next_date.m, next_date.d, dp[next_date.y][next_date.m][next_date.d] ? "true" : "false");
-		}
-		next_date = get_next_month(i, j, k);
-		if(next_date.y <= 101 && next_date.y != -1){
-		       	dp[next_date.y][next_date.m][next_date.d] |= !dp[i][j][k];
-			printf("\t\t\t%d-%02d-%02d %s\n", next_date.y+1900, next_date.m, next_date.d, dp[next_date.y][next_date.m][next_date.d] ? "true" : "false");
-		}
+	Date date(Y, M, D);
+	memset(dp, 0, sizeof(dp));
+	while(date.y < 2001 || date.m < 11 || date.d < 4){
+			
+		Date temp = date + 1;
+		dp[temp.y-1900][temp.m][temp.d] |= !dp[date.y-1900][date.m][date.d];
+		
+		temp = date << 1;
+		dp[temp.y-1900][temp.m][temp.d] |= !dp[date.y-1900][date.m][date.d];
+
 		if(dp[101][11][4]){
 			printf("YES\n");
 			return;
-		}else if( i == 101 && j == 11 && k > 4) break;
-		cout<<endl;
-		days = date_of_month[j] + leap_date(i, j);
-		k = k % days + 1;
-		j = (j + (k == 1) - 1) % 12 + 1;
-		i += (j == 1 && k == 1);
-	}	
-	printf("NO\n");	
+		}
+		date = date + 1;
 
+	}
+	printf("NO\n");
 }
 int main(){
 	int T;
