d8f0d38d4159c06186970c87ee05f878b534abd0
a9502854519 <a9502854519@gmail.com>
2017-10-21 10:00:16 +0800

    UVa 125 solved

--- UVa125.cpp
+++ UVa125.cpp
@@ -1,75 +1,34 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
-#include<vector>
 #define MAX_V 30
 
 
 using namespace std;
 
 int E, V;
-vector<int> G[MAX_V];
-vector<int> rG[MAX_V];
-vector<int> vs;
-bool used[MAX_V];
-int cmp[MAX_V];
 int dp[MAX_V][MAX_V];
 
-void add_edge(int from, int to){
-	G[from].push_back(to);
-	rG[to].push_back(from);
-}
-
-void dfs(int v){
-	used[v] = true;
-	for(int i = 0; i < G[v].size(); i++){
-		if(!used[G[v][i]]) dfs(G[v][i]);
-	}
-	vs.push_back(v);
-}
-
-void rdfs(int v, int k, int& count){
-	used[v] = true;
-	cmp[v] = k;
-	for(int i = 0; i < rG[v].size(); i++){
-		if(!used[rG[v][i]]) rdfs(rG[v][i], k, ++count);
-	}
-	if(count > 1) dp[v][v] = -1;
-}
-
-int scc(){
-	memset(used, 0, sizeof(used));
-	vs.clear();
-
-	for(int v = 0; v < V; v++){
-		if(!used[v]) dfs(v);
-	}
-	memset(used, 0, sizeof(used));
-	int k = 0;
-	for(int i = vs.size() - 1; i >= 0; i--){
-		int count = 1;
-		if(!used[vs[i]]) rdfs(vs[i], k++, count);
-	}
-	return k;
-}
-
-void calc(int i, int u, bool cycle){
-	if(dp[u][u] == -1) used[u] = true;
-	for(int j = 0; j < G[u].size(); j++){
-		int v = G[u][j];
-		if(!used[v] && dp[i][v] != -1){
-			if(cycle || dp[v][v] == -1) dp[i][v] = -1;
-			else dp[i][v]++;
-			calc(i, v, (dp[v][v] == -1 || cycle));
-		}
-	}
-}
 
 void solve(){
-	scc();
-	for(int i = 0; i < V; i++){
-		memset(used, 0, sizeof(used));
-		calc(i, i, (dp[i][i] == -1));
+	for(int k = 0; k < V; k++){
+		for(int j = 0; j < V; j++){
+			for(int i = 0; i < V; i++){
+				dp[i][j] += dp[i][k] * dp[k][j];
+			}
+		}
+	}
+	for(int k = 0; k < V; k++){
+		if(dp[k][k] > 0){
+			for(int i = 0; i < V; i++){
+				for(int j = 0; j < V; j++){
+					if(dp[i][k] != 0 && dp[k][j] != 0){
+						dp[i][j] = -1;
+					}
+				}
+			}
+			dp[k][k] = -1;
+		}
 	}
 	for(int i = 0; i < V; i++){
 		for(int j = 0; j < V; j++){
@@ -77,23 +36,15 @@ void solve(){
 		}
 	}
 }
-
-void init(){
-	memset(dp, 0, sizeof(dp));
-	for(int i = 0; i < MAX_V; i++){
-		G[i].clear();
-		rG[i].clear();
-	}
-}	
 int main(){
 	int c = 0;
 	while(scanf("%d", &E) != EOF){
+		memset(dp, 0, sizeof(dp));
 		V = 0;
-		init();
 		for(int i = 0; i < E; i++){
 			int j, k;
 			cin>>j>>k;
-			add_edge(j, k);
+			dp[j][k]++;
 			V = max(V, j);
 			V = max(V, k);
 		}
