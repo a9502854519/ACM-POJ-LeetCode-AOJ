4d39ec85f5e3757725636579ca89be2c29c6dafa
a9502854519 <a9502854519@yahoo.com.tw>
2017-11-03 09:18:59 +0800

    POJ 3728 solved!!

--- 3728.cpp
+++ 3728.cpp
@@ -16,8 +16,8 @@ int depth[MAX_V];
 int parent[MAX_LOG_V][MAX_V];
 int max_w[MAX_LOG_V][MAX_V];
 int min_w[MAX_LOG_V][MAX_V];
-int max_profit[MAX_LOG_V][MAX_V];
-int min_profit[MAX_LOG_V][MAX_V];
+int up_profit[MAX_LOG_V][MAX_V];
+int down_profit[MAX_LOG_V][MAX_V];
 int w[MAX_V];
 
 
@@ -28,14 +28,14 @@ void add_edge(int u, int v){
 void dfs(int p, int v, int d){
 	//---
 	parent[0][v] = p;
-	min_w[0][v] = (p == -1 ? w[v] : min(w[v], w[p]));
-	max_w[0][v] = (p == -1 ? w[v] : max(w[v], w[p]));
-	if(p == -1 || w[p] - w[v] <= 0) max_profit[0][v] = 0;
-	else max_profit[0][v] = w[p] - w[v];
-
-	if(p == -1 || w[p] - w[v] >= 0) min_profit[0][v] = 0;
-	else min_profit[0][v] = w[p] - w[v];
-
+	min_w[0][v] = max_w[0][v] = w[v];
+	up_profit[0][v] = down_profit[0][v] = 0;
+	if(p != -1){
+		min_w[0][v] = min(w[p], w[v]);
+		max_w[0][v] = max(w[p], w[v]);
+		up_profit[0][v] = (w[p] - w[v] > 0 ? w[p] - w[v] : 0);
+		down_profit[0][v] = (w[v] - w[p] > 0 ? w[v] - w[p] : 0);
+	}
 	depth[v] = d;
 	//---
 	for(int i = 0; i < G[v].size(); i++){
@@ -53,24 +53,17 @@ void init(){
 			    parent[k+1][v] = -1;
 				max_w[k+1][v] = max_w[k][v];
 				min_w[k+1][v] = min_w[k][v];
-				max_profit[k+1][v] = max_profit[k][v];
-				min_profit[k+1][v] = min_profit[k][v];
+				up_profit[k+1][v] = up_profit[k][v];
+				down_profit[k+1][v] = down_profit[k][v];
 			}else{
 			    parent[k+1][v] = parent[k][parent[k][v]];
 				max_w[k+1][v] = max(max_w[k][v], max_w[k][parent[k][v]]);
 				min_w[k+1][v] = min(min_w[k][v], min_w[k][parent[k][v]]);
-				max_profit[k+1][v] = max( max(max_profit[k][v], max_profit[k][parent[k][v]]), 
+				up_profit[k+1][v] = max( max(up_profit[k][v], up_profit[k][parent[k][v]]), 
 						          max_w[k][parent[k][v]] - min_w[k][v]);
-				min_profit[k+1][v] = min( min(min_profit[k][v], min_profit[k][parent[k][v]]),
-						          min_w[k][parent[k][v]] - max_w[k][v]);
-/*				if(k == 0 && v + 1 == 2){
-					printf("min_profit[%d][%d] = %d\n", k+1, v+1, min_profit[k+1][v]);
-					printf("min_profit[%d][%d] = %d\n", k, v+1, min_profit[k][v]);
-					printf("min_profit[%d][%d] = %d\n", k, parent[k][v]+1, min_profit[k][parent[k][v]]);
-					printf("min_w[%d][%d] = %d\n", k, parent[k][v]+1, min_w[k][parent[k][v]]);
-					printf("max_w[%d][%d] = %d\n", k, v+1, max_w[k][v]);
-				}
-*/			}
+				down_profit[k+1][v] = max( max(down_profit[k][v], down_profit[k][parent[k][v]]),
+						          max_w[k][v] - min_w[k][parent[k][v]]);
+			}
 		}
 	}
 }
@@ -79,29 +72,26 @@ void solve(){
 	init();
 	int u, v;
 	scanf("%d", &Q);
-	//for(int k = 0; k < MAX_LOG_V; k++){
-	//	for(int v = 0; v < V; v++){
-	//		printf("min_w[%d][%d] = %d\n", k, v+1, min_w[k][v]);
-	//	}
-	//}
 	while(Q--){
 		scanf("%d %d", &u, &v);
 		u--; v--;
-		int res = -INF, mn = w[u], mx = w[v], lca;
+		int res = -INF, lmn = w[u], lmx = w[u], rmn = w[v], rmx = w[v], lca;
 
 		if(depth[u] > depth[v]){
 			for(int k = 0; k < MAX_LOG_V; k++){
 				if((depth[u] - depth[v]) >> k & 1){
-					res = max(res, max_profit[k][u]);
-					mn = min(mn, min_w[k][u]);
+					lmx = max_w[k][u];
+					res = max(res, max(lmx - lmn, up_profit[k][u]));
+					lmn = min(lmn, min_w[k][u]);
 					u = parent[k][u];
 				}
 			}
 		}else if(depth[u] < depth[v]){
 			for(int k = 0; k < MAX_LOG_V; k++){
 				if((depth[v] - depth[u]) >> k & 1){
-					res = max(res, -min_profit[k][v]);
-					mx = max(mx, max_w[k][v]);
+					rmn = min_w[k][v];
+					res = max(res, max(down_profit[k][v], rmx - rmn));
+					rmx = max(rmx, max_w[k][v]);
 					v = parent[k][v];
 				}
 			}
@@ -110,18 +100,21 @@ void solve(){
 		else{
 			for(int k = MAX_LOG_V - 1; k >= 0; k--){
 				if(parent[k][u] != parent[k][v]){
-					res = max(max(max_profit[k][u], -min_profit[k][v]), res);
-					mn = min(mn, min_w[k][u]);
-					mx = max(mx, max_w[k][v]);
+					lmx = max_w[k][u];
+					rmn = min_w[k][v];
+					res = max(max(up_profit[k][u], down_profit[k][v]), res);
+					res = max(res, max(lmx - lmn, rmx - rmn));
+					lmn = min(lmn, min_w[k][u]);
+					rmx = max(rmx, max_w[k][v]);
 					u = parent[k][u];
 					v = parent[k][v];
 				}
 			}
 			lca = parent[0][u];
 		}
-		mn = min(mn, w[lca]);
-		mx = max(mx, w[lca]);
-		res = max(res, mx - mn);
+		lmn = min(lmn, w[lca]);
+		rmx = max(rmx, w[lca]);
+		res = max(res, rmx - lmn);
 		printf("%d\n", res);
 	}
 }
